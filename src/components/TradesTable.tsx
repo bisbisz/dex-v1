9326d0cbdce3f0cd34b6a900bc86d8f791bf81ae
Crazy Ape Coin <85198323+solcrazyape@users.noreply.github.com>
2021-07-27 09:56:47 -0700

    Update TradesTable.tsx

--- src/components/TradesTable.tsx
+++ src/components/TradesTable.tsx
@@ -30,7 +30,7 @@ export default function PublicTrades({ smallScreen }) {
             }
       }
     >
-      <Title>Recent Market trades</Title>
+      <Title>Recent Ape In</Title>
       <SizeTitle>
         <Col span={8}>Price ({quoteCurrency}) </Col>
         <Col span={8} style={{ textAlign: 'right' }}>
