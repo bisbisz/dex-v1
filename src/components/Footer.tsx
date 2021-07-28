a88c8fe433eeb4faed363af6d73aa622d1920118
Crazy Ape Coin <85198323+solcrazyape@users.noreply.github.com>
2021-07-27 17:20:16 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,15 @@ const { Footer } = Layout;
 const { useBreakpoint } = Grid;
 
 const footerElements = [
-  { description: 'Website', link: helpUrls.website },
-  { description: 'telegram', link: helpUrls.telegram },
+  {
+    description: 'Serum Developer Resources',
+    link: helpUrls.developerResources,
+  },
   { description: 'Discord', link: helpUrls.discord },
-  { description: 'Twitter', link: helpUrls.twitter }
-  { description: 'Explorer', link: helpUrls.solanaBeach },
+  { description: 'Telegram', link: helpUrls.telegram },
+  { description: 'GitHub', link: helpUrls.github },
+  { description: 'Project Serum', link: helpUrls.projectSerum },
+  { description: 'Solana Network', link: helpUrls.solanaBeach },
 ];
 
 export const CustomFooter = () => {
