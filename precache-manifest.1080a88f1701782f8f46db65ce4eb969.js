15f85879843ce336d62bdb5be259d79f98c31ab2
crazyapecoin <solanabigape@gmail.com>
2021-07-30 08:42:49 -0700

    Updates

---
+++ precache-manifest.1080a88f1701782f8f46db65ce4eb969.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae69d2d5eb741a80b9aa20a08831757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad10c084aa249a427b76",
+    "url": "/static/css/2.8e021f0e.chunk.css"
+  },
+  {
+    "revision": "120ada79afac93e900bf",
+    "url": "/static/css/main.bafe348d.chunk.css"
+  },
+  {
+    "revision": "ad10c084aa249a427b76",
+    "url": "/static/js/2.1c5522c7.chunk.js"
+  },
+  {
+    "revision": "958608aaef3e81c9b20a22457416f406",
+    "url": "/static/js/2.1c5522c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120ada79afac93e900bf",
+    "url": "/static/js/main.e88dc169.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "9f1cf41fe692b7db9298fdd53c385b0f",
+    "url": "/static/media/logo.9f1cf41f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.994cb2ef7608f84616d8a125c890ebb3.js"
+  "/precache-manifest.1080a88f1701782f8f46db65ce4eb969.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
