c91c9df2cc62dbc75e266271e1f0f6ddf243a56c
crazyapecoin <solanabigape@gmail.com>
2021-09-17 03:28:09 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18eb8698f7910d427d1bfe3e734c45c7.js"
+  "/precache-manifest.a88ea4dcaf73c170f7f74fd410782729.js"
 );
 
 self.addEventListener('message', (event) => {
